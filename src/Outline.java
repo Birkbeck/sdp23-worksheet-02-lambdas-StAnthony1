eb8187d1e6cc55ec0cfdefe03607599bed5ee72c
O_ONei <ooneil01@student.bbk.ac.uk>
2023-01-26 14:03:52 +0000

    Draft Q2

--- src/Outline.java
+++ src/Outline.java
@@ -1,26 +1,39 @@
 import java.awt.image.AreaAveragingScaleFilter;
 import java.util.Arrays;
 import java.lang.Integer;
+import java.lang.String;
+
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
 
-    String[] fruitBowl = {"Celmentine", "Pineapple", "Lychee", "Fruit1", "Watermelon", "Apple", "Banana"};
+  private static int orderByE(String s1, String s2) {
+    if (s1.contains("e") & !s2.contains("e")) return -1;
+    return 0;
+  }
 
-    Arrays.sort(fruitBowl, (f1, f2) -> Integer.compare(f1.length(), f2.length()));
-    System.out.println(Arrays.asList(fruitBowl));
+    public static void main (String...args){ // varargs alternative to String[]
+      Integer[] intArray = {1, 7, 3, 4, 8, 2};
+      System.out.println(Arrays.asList(intArray));
+      // Arrays.sort(intArray,.......)
 
-    Arrays.sort(fruitBowl, (f1,f2)-> Integer.compare(f2.length(),f1.length()));
-    System.out.println(Arrays.asList(fruitBowl));
+      String[] fruitBowl = {"Celmentine", "Pineapple", "Lychee", "Fruit1", "Watermelon", "Apple", "Banana"};
 
-    Arrays.sort(fruitBowl, (f1,f2) -> Integer.compare(f1.charAt(0), f2.charAt(0)));
-    System.out.println(Arrays.asList(fruitBowl));
+      Arrays.sort(fruitBowl, (f1, f2) -> Integer.compare(f1.length(), f2.length()));
+      System.out.println(Arrays.asList(fruitBowl));
 
-    Arrays.sort(fruitBowl, (f1,f2) -> {if (f1.contains("e") & !f2.contains("e")) return -1; return 0;});
-    System.out.println(Arrays.asList(fruitBowl));
+      Arrays.sort(fruitBowl, (f1, f2) -> Integer.compare(f2.length(), f1.length()));
+      System.out.println(Arrays.asList(fruitBowl));
 
-  } 
+      Arrays.sort(fruitBowl, (f1, f2) -> Integer.compare(f1.charAt(0), f2.charAt(0)));
+      System.out.println(Arrays.asList(fruitBowl));
+
+      //Arrays.sort(fruitBowl, (f1, f2) -> {
+        //if (f1.contains("e") & !f2.contains("e")) return -1;
+        //return 0;
+     // });
+      System.out.println(Arrays.asList(fruitBowl));
+
+    Arrays.sort(fruitBowl, Outline::orderByE);
+      System.out.println(Arrays.asList(fruitBowl));
+    }
 }
