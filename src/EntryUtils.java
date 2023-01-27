77ea1ec68743a4bfcab88b8e3aa8a771bd030602
O_ONei <ooneil01@student.bbk.ac.uk>
2023-01-27 10:15:52 +0000

    Q5 completed

--- src/EntryUtils.java
+++ src/EntryUtils.java
@@ -1,19 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntryUtils {
     static String betterString(String s1, String s2, TwoStringPredicate f) {
         if (f.better(s1, s2)) return s1;
         return s2;
     }
 
-    static String betterEntry(String s1, String s2, TwoGenericPredicate<String> f) {
+    static String betterEntry(String s1, String s2, TwoGenericPredicate f) {
         if (f.betterEntry(s1, s2)) return s1;
         return s2;
     }
 
+    static List<String> allMatches(List<String> l, Predicate<String> pred){
+        l.removeIf(s -> !pred.f(s));
+        return l;
+    }
+
     public static void main(String[] args) {
+
+        List<String> greetings = new ArrayList<>(List.of("Hi", "Hello", "G'day", "How do?"
+                ,"alright?", "ciao", "Hey", "Hey Jude!" ));
 
         TwoGenericPredicate<String> longest = (a, b) -> a.length() >= b.length();
         //System.out.println(betterString("Hi", "Hello", longest));
         System.out.println(betterEntry("Bonjour", "How are you?", longest));
+
+        //Q5
+        System.out.println(allMatches(greetings,(s)-> s.contains("e")));
 
 
     }
---
+++ src/Predicate.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface Predicate <T>{
+    boolean f(T item);
+}
